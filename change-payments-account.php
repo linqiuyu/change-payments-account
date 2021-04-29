b7cef18648cbb40ea1f94ec21a7e0df31fe1ba72
linqiuyu <qiuyu_lin@fzzixun.com>
2021-04-29 14:16:02 +0800

    修复参数命名错误

--- change-payments-account.php
+++ change-payments-account.php
@@ -19,7 +19,7 @@ if ( ! defined( 'CPY_PLUGIN_FILE' ) ) {
     define( 'CPY_PLUGIN_FILE', __FILE__ );
 }
 
-if ( ! defined( 'PF_PLUGIN_DIR' ) ) {
+if ( ! defined( 'CPY_PLUGIN_DIR' ) ) {
     define( 'CPY_PLUGIN_DIR', plugin_dir_path( CPY_PLUGIN_FILE ) );
 }
 
